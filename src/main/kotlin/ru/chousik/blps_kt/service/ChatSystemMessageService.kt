4ed5b0d02f5671549a21cb77130c8a3acb164d0b
hipeoplea <hipeoplea@yandex.ru>
2026-03-20 23:34:10 +0300

    some fixes

--- src/main/kotlin/ru/chousik/blps_kt/service/ChatSystemMessageService.kt
+++ src/main/kotlin/ru/chousik/blps_kt/service/ChatSystemMessageService.kt
@@ -2,14 +2,17 @@ package ru.chousik.blps_kt.service
 
 import java.time.OffsetDateTime
 import java.util.UUID
+import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Service
+import ru.chousik.blps_kt.api.chat.ChatMessageResponse
 import ru.chousik.blps_kt.model.Chat
 import ru.chousik.blps_kt.model.ChatMessage
 import ru.chousik.blps_kt.repository.ChatMessageRepository
 
 @Service
 class ChatSystemMessageService(
-    private val chatMessageRepository: ChatMessageRepository
+    private val chatMessageRepository: ChatMessageRepository,
+    private val messagingTemplate: SimpMessagingTemplate
 ) {
 
     fun append(chat: Chat, message: String) {
@@ -20,6 +23,7 @@ class ChatSystemMessageService(
             this.message = message
             createdAt = OffsetDateTime.now()
         }
-        chatMessageRepository.save(entity)
+        val saved = chatMessageRepository.save(entity)
+        messagingTemplate.convertAndSend("/topic/chats/${chat.id}", ChatMessageResponse.from(saved))
     }
 }
